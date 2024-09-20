05cf5e7de29056adcefbcfa390c67da978e82a6f
Thiago Cadamuro <ibst.thiago@gmail.com>
2024-09-19 22:38:04 -0300

    chore: new changes

---
+++ S03_FirstStepsInJUnit5WithJava/src/test/java/br/com/cadamuro/FooBarTest.java
@@ -0,0 +1,15 @@
+package br.com.cadamuro;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FooBarTest {
+
+	@Test
+	void test() {
+		//("Not yet implemented");
+		System.out.println("Hello JUnit 5");
+	}
+
+}
---
+++ S03_FirstStepsInJUnit5WithJava/src/test/java/br/com/math/SimpleMathTest.java
@@ -0,0 +1,17 @@
+package br.com.math;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SimpleMathTest {
+
+	@Test
+	void testSum() {
+		SimpleMath math = new SimpleMath();
+		Double actual = math.sum(6.2D, 2D);
+		double expected = 8.2D;
+		assertEquals(expected, actual, "6.2 + 2 did not produce expected result!");
+	}
+
+}
